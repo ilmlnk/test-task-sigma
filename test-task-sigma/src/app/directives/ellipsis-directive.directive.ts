23d4a9c1d672e1f9de283e1bb67151946288885f
ilmlnk <ilmlnkcorp@gmail.com>
2024-09-11 00:46:15 +0200

    adding responsive design

--- test-task-sigma/src/app/directives/ellipsis-directive.directive.ts
+++ test-task-sigma/src/app/directives/ellipsis-directive.directive.ts
@@ -1,34 +1,53 @@
-import { Directive, ElementRef, HostListener, Input, Renderer2, AfterViewInit } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input, Renderer2, AfterViewInit, OnDestroy } from '@angular/core';
 
 @Directive({
   selector: '[appEllipsis]',
   standalone: true
 })
-export class EllipsisDirective implements AfterViewInit {
+export class EllipsisDirective implements AfterViewInit, OnDestroy {
   @Input() lines: number = 2;
+  private resizeObserver: ResizeObserver;
 
   constructor(private el: ElementRef, private renderer: Renderer2) { }
+
 
   @HostListener('window:resize') 
   onResize() {
     this.applyEllipsis();
   }
 
-  ngAfterViewInit() {
-    this.applyEllipsis();
+  ngOnDestroy() {
+    if (this.resizeObserver) {
+      this.resizeObserver.disconnect();
+    }
   }
 
-  applyEllipsis() {
-    const element = this.el.nativeElement;
-    const lineHeight = parseInt(getComputedStyle(element).lineHeight, 10);
-    const maxHeight = lineHeight * this.lines;
+  ngAfterViewInit() {
+    this.applyEllipsis();
+    this.setupResizeObserver();
+  }
 
-    if (element.scrollHeight > maxHeight) {
-      let text = element.innerText;
-      while (element.scrollHeight > maxHeight) {
-        text = text.slice(0, -1);
-        element.innerText = text + '...';
+  private applyEllipsis() {
+    const element = this.el.nativeElement;
+    if (typeof window !== 'undefined' && typeof getComputedStyle === 'function') {
+      const style = getComputedStyle(element);
+      const lineHeight = parseInt(style.lineHeight, 10);
+      const maxHeight = lineHeight * this.lines;
+
+      if (element.scrollHeight > maxHeight) {
+        let text = element.innerText;
+        while (element.scrollHeight > maxHeight && text.length > 0) {
+          text = text.slice(0, -1);
+          element.innerText = text + '...';
+        }
       }
+    }
+  }
+
+  private setupResizeObserver() {
+    if (typeof ResizeObserver !== 'undefined') {
+      this.resizeObserver = new ResizeObserver(() => this.applyEllipsis());
+      this.resizeObserver.observe(this.el.nativeElement);
     }
   }
 }
